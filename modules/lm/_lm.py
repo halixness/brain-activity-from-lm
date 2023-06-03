7a4a340e80b8ea496ef3006009126e5db9f466a6
Diego Calanzone <xdiego.wp@hotmail.com>
2023-06-03 14:08:18 +0200

    reproduced one Mitchell et al. 2008 result

--- modules/lm/_lm.py
+++ modules/lm/_lm.py
@@ -18,6 +18,17 @@ class LanguageModel:
         self.tokenizer.model_max_length = 512
         self.model.eval()
 
+    def get_noncontextualized_embeddings(self, word, layer=-1):
+        """ Returns the embedding of a word without context """
+        tokenized = self.tokenizer(word, return_tensors="pt").input_ids.to(self.device)
+        layer_activations = self.model(tokenized)[2][1:][layer][0]
+        word_embedding = layer_activations[1:-1].mean(dim=0).detach().cpu().numpy()
+        
+        del layer_activations
+        del tokenized
+        torch.cuda.empty_cache()
+        return word_embedding
+
     def get_contextualized_embeddings(self, word, sentences, batch_size=32, progress=False):
         """ Batch process a set of contexts for a word to compute its embedding 
             
@@ -60,7 +71,7 @@ class LanguageModel:
         return contextualized_words
     
     
-    def get_dominant_meaning(self, word_embeddings, max_K = 20):
+    def get_dominant_meaning(self, word_embeddings, max_meanings = 20):
         """ Apply K-means clustering from a set of contextualized word embeddings, return centroid of dominant meaning
             
             input:      word_embeddings     np.array:(N, 768)   Multiple contextual N embeddings of the same word, embeded size 768
@@ -71,7 +82,7 @@ class LanguageModel:
         # Choose best no.meanings
         max_k = None
         max_score = -1
-        for k in list(range(2, max_K)):
+        for k in list(range(2, min(len(word_embeddings)-1, max_meanings))):
             kmeans = KMeans(n_clusters=k, random_state=42, n_init="auto").fit(word_embeddings)
             score = silhouette_score(word_embeddings, kmeans.labels_)
             if score > max_score: 
